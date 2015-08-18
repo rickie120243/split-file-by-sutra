43ee818d54224acbf5d34fcc9157b542c04639d6
rickie120243 <rickie120243@gmail.com>
2015-08-18 13:15:27 +0800

    split done

---
+++ splitSutra.js
@@ -0,0 +1,15 @@
+var fs=require("fs");
+var filename="phyagchen001(with pb)";
+var fn=fs.readFileSync("./"+filename+".xml","utf8");
+
+var splitbySutra=function(m){
+	var content=m.replace(/<sutra id/g,"#<sutra id")
+				.replace(/<pb id="1.1b"\/>\r?\n#<sutra/g,'#<pb id="1.1b"/>\n<sutra');
+	var arr=content.split("#");
+	arr.map(function(sutra){
+		sutra.replace(/M\d+/,function(id){
+			fs.writeFileSync("./splited/"+filename+"_sutra"+id+".xml",sutra,"utf8");
+		});
+	});
+}
+splitbySutra(fn);
