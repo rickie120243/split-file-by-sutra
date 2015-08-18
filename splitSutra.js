6e4e485f187579d6ae5eb74731edb76eef8abd03
rickie120243 <rickie120243@gmail.com>
2015-08-18 18:08:12 +0800

    add list

--- splitSutra.js
+++ splitSutra.js
@@ -1,17 +1,24 @@
 var fs=require("fs");
-var filename="phyagchen001(with pb)";
-var fn=fs.readFileSync("./"+filename+".xml","utf8");
+var list=fs.readFileSync("./data_list.txt","utf8").split(/\r?\n/);
 
-var splitbySutra=function(m){
+var splitbySutra=function(m,fn){
 	var content=m.replace(/<sutra id/g,"#<sutra id")
-				.replace(/<pb id="1.1b"\/>\r?\n#<sutra/g,'#<pb id="1.1b"/>\r\n<sutra');
+				.replace(/<pb id="1.1b"\/>\r?\n#<sutra/g,'#<pb id="1.1[ab]"/>\r\n<sutra');
+	fn=fn.replace(/.xml/g,"");
 	var arr=content.split("#");
 	arr.map(function(sutra){
 		sutra.replace(/M\d+/,function(id){
-			fs.writeFileSync("./splited/"+filename+"_sutra"+id+".xml",sutra,"utf8");
+			fs.writeFileSync("./splited/"+fn+"_sutra"+id+".xml",sutra,"utf8");
 		});
 	});
 }
+
+var readFile=function(fn){
+	var content=fs.readFileSync("./data/"+fn,"utf8");
+	splitbySutra(content,fn);
+}
 var t=new Date();
-splitbySutra(fn);
+list.map(readFile);
 console.log(new Date()-t);
+//splitbySutra(fn);
+//
