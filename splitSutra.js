6625290bbf23defb469b2129e02e2d428fcd0a89
rickie120243 <rickie120243@gmail.com>
2015-08-19 13:39:48 +0800

    first pb replace

--- splitSutra.js
+++ splitSutra.js
@@ -5,14 +5,14 @@ var list=fs.readFileSync("./data/data_list.txt","utf8").split(/\r?\n/);
 
 var writeFile=function(path, contents, cb) {
   mkdirp(getDirName(path), function (err) {
-    if (err) return cb(err)
+    if (err) return cb(err);
     fs.writeFileSync(path,contents,"utf8");
   });
 } 
 
 var splitbySutra=function(m,fn){
 	var content=m.replace(/<sutra id/g,"#<sutra id")
-				.replace(/<pb id="1.1[ab]"\/>\r?\n#<sutra/g,'#<pb id="1.1b"/>\r\n<sutra'); //
+				.replace(/<pb id="(1.\d+[ab])"\/>\r?\n#<sutra/,'#<pb id="$1"/>\r\n<sutra'); //
 	var name=fn.split(".");
 	var arr=content.split("#");
 	arr.map(function(sutra){
